2104a6ccdbdd9ebc32a2dfbdd94d86e526aba656
James Lappin <james.lappin@checkout.com>
2021-02-25 18:01:03 +0000

    Firs line of the input

--- src/HashCode/Challenge.cs
+++ src/HashCode/Challenge.cs
@@ -2,6 +2,10 @@ namespace HashCode
 {
     public class Challenge
     {
-        
+        public int Duration { get; set; }
+        public int NumberOfIntersections { get; set; }
+        public int NumberOfStreets { get; set; }
+        public int NumberOfCars { get; set; }
+        public int BonusPoints { get; set; }
     }
 }
--- src/HashCode/Input.cs
+++ src/HashCode/Input.cs
@@ -1,9 +1,24 @@
+using System.IO;
+using System.Linq;
+
 namespace HashCode
 {
     public static class Input
     {
         public static Challenge Parse(string filePath)
         {
+            var lines = File.ReadLines(filePath).ToList();
+
+            var firstLine = lines[0].Split(' ');
+            new Challenge
+            {
+                Duration = int.Parse(firstLine[0]),
+                NumberOfIntersections = int.Parse(firstLine[1]),
+                NumberOfStreets = int.Parse(firstLine[2]),
+                NumberOfCars = int.Parse(firstLine[3]),
+                BonusPoints = int.Parse(firstLine[4]),
+            };
+
             return new Challenge();
         }
     }
