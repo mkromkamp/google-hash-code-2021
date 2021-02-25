d6fe499b11c2fcb8ca59a336c34b85befe5ac57c
Martin Kromkamp <m.kromkamp@gmail.com>
2021-02-25 17:14:57 +0000

    Skeleton

---
+++ src/HashCode/Input.cs
@@ -0,0 +1,10 @@
+namespace HashCode
+{
+    public static class Input
+    {
+        public static Challenge Parse(string filePath)
+        {
+            return new Challenge();
+        }
+    }
+}
---
+++ src/HashCode/Solution.cs
@@ -0,0 +1,15 @@
+namespace HashCode
+{
+    public class Solution
+    {
+        public long GetScore()
+        {
+            return 0;
+        }
+        
+        public static Solution Solve(Challenge challenge)
+        {
+            return new Solution();
+        }
+    }
+}
